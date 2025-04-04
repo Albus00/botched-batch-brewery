7a8d630a2f4d0ec5474ba50ebdc66d64a460d460
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2025-04-04 21:56:53 +0200

    Add item and inventory system with pickup functionality

---
+++ Scripts/Interactable/item_stack.gd
@@ -0,0 +1,7 @@
+class_name ItemStack
+
+var item: Item:
+	set(value):
+		item = value
+
+var count: int
---
+++ Scripts/Player/player_inventory.gd
@@ -0,0 +1,5 @@
+class_name PlayerInventory
+
+const SIZE := 4
+
+var items: Array[ItemStack] = []
