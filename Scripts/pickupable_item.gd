82094e8ed49e31d0df55915e9d4eaeb7c6bb5fe3
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2025-04-04 21:56:11 +0200

    merged stashed changes with main branch

--- Scripts/pickupable_item.gd
+++ Scripts/pickupable_item.gd
@@ -1,13 +1,23 @@
 extends Node
 
+@export var item_info: Item
+
 var interactPanel: Panel
 
+
+@onready var sprite2D: Sprite2D = $Sprite2D
+@onready var area2D: Area2D = $Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	interactPanel = get_node("Control/Panel") as Panel
 	interactPanel.visible = false
 
+	print(item_info.name)
+	print(item_info.description)
+	print(item_info.sprite)
+	Sprite2D.texture = item_info.sprite # TODO: Fix this conversion, maybe https://www.reddit.com/r/godot/comments/1ahsk5o/swapping_sprite2ds_atlastexture_in_code/
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
